3f0485693eaf88e79a03e13c77654eba26fcd78b
Alex <maystruks08@gmail.com>
2021-10-19 13:53:20 +0300

    [NF] Changed icon

--- app/src/main/java/com/hssoft/equipmentaccounting/ui/MainActivity.kt
+++ app/src/main/java/com/hssoft/equipmentaccounting/ui/MainActivity.kt
@@ -3,7 +3,7 @@ package com.hssoft.equipmentaccounting.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
@@ -11,12 +11,12 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.hssoft.equipmentaccounting.ui.debtors.DebtorsScreen
 import com.hssoft.equipmentaccounting.ui.home.HomeScreen
-import com.hssoft.equipmentaccounting.ui.home.HomeScreenViewModel
 import com.hssoft.equipmentaccounting.ui.rent.RentScreen
 import com.hssoft.equipmentaccounting.ui.theme.EquipmentAccountingTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
+@ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
